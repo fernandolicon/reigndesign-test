b58d0ca4efb44c5949cd3a68864b51d65283ee54
fernandolicon <wisimata@gmail.com>
2015-09-30 17:07:40 -0600

    Initial Commit

---
+++ ReignDesignTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReignDesignTest
+//
+//  Created by Luis Fernando Mata on 30/9/15.
+//  Copyright © 2015 Luis Fernando Mata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ReignDesignTestTests/ReignDesignTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  ReignDesignTestTests.swift
+//  ReignDesignTestTests
+//
+//  Created by Luis Fernando Mata on 30/9/15.
+//  Copyright © 2015 Luis Fernando Mata. All rights reserved.
+//
+
+import XCTest
+@testable import ReignDesignTest
+
+class ReignDesignTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
